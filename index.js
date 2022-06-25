930ad2fec0353112cf8317c1391359a45b0536be
Rodrob <rodrigo.22.11.88@gmail.com>
2022-06-25 15:39:48 -0400

    FRONT 50%

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require("express")
 const app = express()
+const bodyParser = require("body-parser")
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
+
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
 
 app.get("/",function(req,res){
     res.render("t-1")
@@ -10,6 +14,39 @@ app.get("/",function(req,res){
 
 app.get("/cadastro",function(req,res){
     res.render("t-2")
+})
+
+app.get("/teste",function(req,res){
+    res.render("t-teste")
+})
+
+app.post("/salvarcadastro",(req, res) => {
+
+    // TIPO DE PESSOA (falta)
+
+    //DADOS PESSOAIS
+    let nome = req.body.nome
+    let cpfCnpj = req.body.cpfCnpj
+    let cel = req.body.cel
+    let nascimento = req.body.nascimento
+    let email = req.body.email
+    let senha = req.body.senha
+    
+    //ENDEREÇO
+    let cep = req.body.cep
+    let logradouro = req.body.logradouro
+    let numero = req.body.numero
+    let complemento = req.body.complemento
+    let bairro = req.body.bairro
+    let cidade = req.body.cidade
+    let uf = req.body.uf
+
+
+    res.send(tipoPessoa)
+})
+
+app.post("/resposta",(req, res) => {
+    res.send("Formulário TESTE recebido!")
 })
 
 app.listen(5000,()=>{
